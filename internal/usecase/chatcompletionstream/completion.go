850ac583b0a10b4659c25c936cd43fd73f300744
David Ribeiro <daviddev.code@gmail.com>
2023-04-20 23:51:17 -0300

    initial commit

---
+++ internal/usecase/chatcompletionstream/completion.go
@@ -0,0 +1,131 @@
+package chatcompletionstream
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	"github.com/devfullcycle/fclx/chatservice/internal/domain/entity"
+	"github.com/devfullcycle/fclx/chatservice/internal/domain/gateway"
+	"github.com/sashabaranov/go-openai"
+)
+
+type ChatCompletionConfigInputDTO struct {
+	Model                string
+	ModelMaxTokens       int
+	Temperature          float32
+	TopP                 float32
+	N                    int
+	Stop                 []string
+	MaxTokens            int
+	PresencePenalty      float32
+	FrenquencyPenalty    float32
+	InitialSystemMessage string
+}
+
+type ChatCompletionInputDTO struct {
+	ChatId      string
+	UserId      string
+	UserMessage string
+	Config      ChatCompletionConfigInputDTO
+}
+
+type ChatCompletionOutputDTO struct {
+	ChatId  string
+	UserId  string
+	Content string
+}
+
+type ChatCompletionUseCase struct {
+	ChatGateway  gateway.ChatGateway
+	OpenAiClient *openai.Client
+}
+
+func NewChatCompletion(chatGateway gateway.ChatGateway, openAiClient *openai.Client) *ChatCompletionUseCase {
+	return &ChatCompletionUseCase{
+		ChatGateway:  chatGateway,
+		OpenAiClient: openAiClient,
+	}
+}
+
+func (uc *ChatCompletionUseCase) Execute(ctx context.Context, input ChatCompletionInputDTO) (*ChatCompletionOutputDTO, error) {
+	chat, err := uc.ChatGateway.FindChatById(ctx, input.ChatId)
+	if err != nil {
+		if err.Error() == "chat not found" {
+			chat, err = createNewChat(input)
+			if err != nil {
+				return nil, errors.New("error creating new chat:" + err.Error())
+			}
+			err = uc.ChatGateway.CreateChat(ctx, chat)
+			if err != nil {
+				return nil, errors.New("error persisting new chat:" + err.Error())
+			}
+		} else {
+			return nil, errors.New("error fetching existing chat:" + err.Error())
+		}
+	}
+	userMessage, err := entity.NewMessage("user", input.UserMessage, chat.Config.Model)
+	if err != nil {
+		return nil, errors.New("error creating user message:" + err.Error())
+	}
+	err = chat.AddMessage(userMessage)
+	if err != nil {
+		return nil, errors.New("error adding user message to chat:" + err.Error())
+	}
+
+	messages := []openai.ChatCompletionMessage{}
+	for _, message := range chat.Messages {
+		messages = append(messages, openai.ChatCompletionMessage{
+			Role:    message.Role,
+			Content: message.Content,
+		})
+	}
+
+	resp, err := uc.OpenAiClient.CreateChatCompletionStream(
+		ctx,
+		openai.ChatCompletionRequest{
+			Model:            chat.Config.Model.Name,
+			Messages:         messages,
+			MaxTokens:        chat.Config.MaxTokens,
+			Temperature:      chat.Config.Temperature,
+			TopP:             chat.Config.TopP,
+			PresencePenalty:  chat.Config.PresencePenalty,
+			FrequencyPenalty: chat.Config.FrequencyPenalty,
+			Stop:             chat.Config.Stop,
+			Stream:           true,
+		},
+	)
+	if err != nil {
+		return nil, errors.New("error creating chat completion:" + err.Error())
+	}
+
+	var fullResponse strings.Builder
+
+	for {
+		response, err := resp.Recv()
+	}
+}
+
+func createNewChat(input ChatCompletionInputDTO) (*entity.Chat, error) {
+	model := entity.NewModel(input.Config.Model, input.Config.ModelMaxTokens)
+	chatConfig := &entity.ChatConfig{
+		Temperature:      input.Config.Temperature,
+		TopP:             input.Config.TopP,
+		N:                input.Config.N,
+		Stop:             input.Config.Stop,
+		MaxTokens:        input.Config.MaxTokens,
+		PresencePenalty:  input.Config.PresencePenalty,
+		FrequencyPenalty: input.Config.FrenquencyPenalty,
+		Model:            model,
+	}
+
+	initialMessage, err := entity.NewMessage("system", input.Config.InitialSystemMessage, model)
+	if err != nil {
+		return nil, errors.New("error creating initial system message:" + err.Error())
+	}
+	chat, err := entity.NewChat(input.UserId, initialMessage, chatConfig)
+	if err != nil {
+		return nil, errors.New("error creating new chat:" + err.Error())
+	}
+	return chat, nil
+}
